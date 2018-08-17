3ef8eadedf27eda9daa11f7bf9336ad3ea3c9f95
Ben Follis <bfollis@uclusion.com>
2018-08-16 19:46:24 -0700

    Fixed up typos and problems exposed by node usage of SDK

--- src/uclusion.js
+++ src/uclusion.js
@@ -5,22 +5,23 @@ import a_client from './components/fetchClient.js';
 
 function Uclusion() {
 
-
     /**
      * Constructs an api client from a given base endpoint url and a user token gotten from cognito
-     * @param configuration a js object contaning baseURL, and the authorizor object wich supports an authorize() method with promises
+     * @param configuration a js object contaning baseURL, and the authorizor object which returns a promise
+     * that resolves to the authorization token
      * @returns a promise that when resolved results in instantiated api client.
      */
-    this.constructClient = (configuration) => {
-        let transportClient = a_client({baseURL: apiBaseUrl});
-        return authorizer.authorize((result) => {
+        this.constructClient = (configuration) => {
+        let transportClient = a_client({baseURL: configuration.baseURL});
+        let authorizerPromise = configuration.authorizer.authorize();
+        return authorizerPromise.then((userToken) => {
             transportClient.setAuthorization(userToken);
             let apiClient = {
                 users: a_users(transportClient),
                 markets: a_markets(transportClient),
                 investibles: a_investibles(transportClient)
             };
-            // console.log(apiClient.user);
+            //console.log(apiClient.user);
             return apiClient;
         });
     };
