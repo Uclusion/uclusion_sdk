bdb68de12a7062720ef05b55d62995615b9d8182
disrael <disrael@uclusion.com>
2019-01-19 06:19:46 -0800

    Only try re-auth on 401. Otherwise swallowing errors.

--- src/components/fetchClient.js
+++ src/components/fetchClient.js
@@ -3,9 +3,8 @@ export function FetchClient(passedConfig){
     let configuration = Object.assign({}, passedConfig);
 
     let responseHandler = (response) => {
-        // if we're 400, 401 or 403 we're unauthorized so jsut tell the authorizer to reauthorize us
-        // and abandon the current call
-        if (response.status === 400 || response.status == 401 || response.status === 403){
+        // if we're 401 we're unauthorized so tell the authorizer to reauthorize us and abandon the current call
+        if (response.status === 401){
             configuration.authorizer.reauthorize() // no page or dest url since I don't have it handy
         } else if (!response.ok) {
             throw response; //give the response to upstream code
