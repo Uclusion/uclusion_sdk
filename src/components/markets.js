98b25e8b63cf61dead43ffc2e82a523f6b2830ed
Ben Follis <bfollis@uclusion.com>
2019-04-01 18:15:32 -0700

    Feat: Updated comments so that we don't pass in args that aren't used, and pass in args that are needed

--- src/components/markets.js
+++ src/components/markets.js
@@ -85,8 +85,6 @@ export function Markets(client){
      *  <li>description: string, <b>required</b></li>
      *  <li>trending_window: number</li>
      *  <li>manual_roi: boolean</li>
-     *  <li>initial_next_stage: string, <b>required</b></li>
-     *  <li>initial_next_stage_threshold: number, <b>required</b></li>
      *  <li>new_team_grant: number of shares to grant to a new team when they enter the account</li>
      *  <li>new_user_grant: number of shares to grant to a new team when they enter the account</li>
      *  <li>investment_bonus_threshold: number of shares the user has to invest to cross into an "active" user</li>
@@ -115,8 +113,7 @@ export function Markets(client){
      *  <li>name : string, <b>required</b></li>
      *  <li>description: string, <b>required</b></li>
      *  <li>trending_window: number, <b>required</b></li>
-     *  <li>initial_next_stage: string, <b>required</b></li>
-     *  <li>initial_next_stage_threshold: number, <b>required</b></li>
+     *  <li>initial_stage: string, <b>required</b>: Id of the initial stage for the market</li>
      * </ul>
      * @param marketId Market to update
      * @param marketUpdateOptions the options for the market
